6d09c2d173de790085e724a28cc941cc45b9c4bb
jin9173 <jinhappier2@gmail.com>
2019-12-11 16:08:49 +0900

    django_extensions 설치 및 requirements.txt 생성

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # PYPI 패키지
+    # shell_plus를 쓰기 위한 library
+    'django_extensions',
+
     # 이 패키지는 application 취급
     'blog',
 ]
