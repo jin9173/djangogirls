a0dff865970964d801e5a17e5f88450e5b552d62
jin9173 <jinhappier2@gmail.com>
2019-12-12 16:03:20 +0900

    url 태그 사용

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,6 @@ from blog.views import post_list, post_detail
 urlpatterns = [
     path('admin/', admin.site.urls),
     # post-list라는 URL에 온 요청은 blog.views.post_list함수가 처리한다.
-    path('post-list/', post_list),
-    path('post-detail/<int:pk>/', post_detail),
+    path('posts/', post_list, name='url-name-post-list'),
+    path('post-detail/<int:pk>/', post_detail, name='url-name-post-detail'),
 ]
