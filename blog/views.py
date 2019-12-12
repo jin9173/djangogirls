ecc14f4be2be640962f3d11f52b19a2125799b4f
jin9173 <jinhappier2@gmail.com>
2019-12-12 13:21:43 +0900

    post_detail view, urls, template연결

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
+import os
+
 from django.shortcuts import render
 
 from blog.models import Post
@@ -24,6 +26,12 @@ def post_list(request):
     return render(request, 'post_list.html', context)
 
 
+def post_detail(request):
+    # URL:      /post-detail/
+    # View:     post_detail (이 함수)
+    # Template: post_detail.html
+    #   내용으로 <h1>Post Detail!</h1>을 갖도록 함
+    return render(request, 'post_detail.html')
 
 
 
